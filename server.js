10c94cd7c151b5af29d489a517257c3308463845
Ben Brandt <benjamin.j.brandt@gmail.com>
2017-05-06 14:33:29 -0700

    Bump cache length to 1 day

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const handle = app.getRequestHandler();
 // This is where we cache our rendered HTML pages
 const ssrCache = new LRUCache({
   max: 100,
-  maxAge: 1000 * 60 * 60 // 1 hour
+  maxAge: 1000 * 60 * 60 * 24 // 1 Day
 });
 
 /*
